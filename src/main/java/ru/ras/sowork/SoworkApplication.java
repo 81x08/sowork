062e5741efc2b27a179ef9eb7a1560458530b042
81x08 <81x08@mail.ru>
2019-06-16 10:22:52 +0500

    Initial commit

---
+++ src/main/java/ru/ras/sowork/SoworkApplication.java
@@ -0,0 +1,13 @@
+package ru.ras.sowork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoworkApplication.class, args);
+	}
+
+}
